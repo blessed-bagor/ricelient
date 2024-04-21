e692bcb7ba3f4c800d8406c83e7680b8682b65a8
Blessed Bagor <blessed@bagor.tech>
2024-04-21 23:59:21 +0800

    Added Google Fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      
+      <div>
+        <h1>Hello Ricelient Farmer!</h1>
+      </div>
     </main>
   );
 }
